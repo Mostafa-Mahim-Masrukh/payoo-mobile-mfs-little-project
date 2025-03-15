bb69952c9fab7f5d6bd6152bdd3785b11144ff85
mostafa <mostafamahimakash384@gmail.com>
2025-03-15 21:50:54 +0600

    add button , cash out

--- js/home.js
+++ js/home.js
@@ -29,3 +29,35 @@ document.getElementById('add-money-button').addEventListener('click', function (
         alert('Your Pin does not match. Try again !');
     }
 })
+
+
+
+
+//cash out money
+
+document.getElementById('withdraw-money-button').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    //getting the PIN code
+    const thePinCode = document.getElementById('the-pin-code').value;
+
+
+
+    //getting the amount of Money
+    const amountOfMoney = document.getElementById('amount-of-money').value;
+
+    //verify the PIN number 
+    //wrong way to verify the password or pin number
+    if (thePinCode === '1234') {
+        const availableBanalce = document.getElementById('available-balance').innerText;
+
+        if (availableBanalce >= amountOfMoney) {
+            const newBalance = parseInt(availableBanalce) - parseInt(amountOfMoney);
+            document.getElementById('available-balance').innerText = newBalance;
+        }
+
+    }
+    else {
+        alert('Your Pin does not match. Try again !');
+    }
+})
